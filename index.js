cd84dfcd0ac6c67f20701b119bc1c3d2302b1f3f
Petar Šegović <petar.segovic1@gmail.com>
2023-10-26 10:50:44 +0200

    Dodan EJS

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const req = require("express/lib/request");
 const app = express();
 const port = 8080;
 
+app.set("view engine","ejs");
+
 app.get("/", (request, response) => {
-     response.send("I am alive!")});
+     let result = {
+          name: "Ivo"
+     }
+     response.render("index",result)});
 
 app.listen(port, () => {console.log("I am listening...")});
